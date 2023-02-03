bfcb45932675acf5635e4a3b6227310eee19e4c8
Catherine Grey <Kaaatrinakuz@gmail.com>
2023-02-03 16:31:07 -0500

    Initial commit.

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import allure
+import pytest
+from allure_commons.types import AttachmentType
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture()
+def browser():
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    # wait 10 seconds to pull the DOM
+    browser.implicitly_wait(10)
+    # maximize browser window to full screen
+    browser.maximize_window()
+    yield browser
+    # make a screenshot before closing the browser
+    allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    # when test is done, close ALL windows of the browser
+    browser.quit()
---
+++ test_search_multiple.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+expected_title = 'Amazon.com. Spend less. Smile more.'
+base_url = 'https://www.amazon.com'
+
+
+@pytest.mark.parametrize("item", [
+    "nike air max",
+    "reebok crossfit shoes men",
+    "puma sneakers",
+    "adidas classic shoes"])
+@pytest.mark.regressiontest
+def test_search(browser, item):
+    # navigate to Amazon.com home page
+    browser.get(base_url)
+    # verify that website title is Amazon.com
+    assert browser.title == expected_title
+    # locate search field element and enter search item from the list in the search field
+    browser.find_element(By.ID, "twotabsearchtextbox").send_keys(item)
+    # locate and click on search button
+    browser.find_element(By.XPATH, "//input[@value='Go']").click()
+    # verify the page title
+    assert browser.title == 'Amazon.com : ' + item
